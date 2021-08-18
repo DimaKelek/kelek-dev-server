16481598f44cae96502b2c40cb0f006e11888de1
Dmitry Kelek <kelekofficial@gmail.com>
2021-08-18 20:28:46 +0300

    init

---
+++ src/Features/SendMessage/send.ts
@@ -0,0 +1,37 @@
+import nodemailer from "nodemailer";
+
+type RequestBodyType = {
+    name: string
+    email: string
+    subject: string
+    message?: string
+}
+
+let smtp_login = process.env.SMTP_LOGIN || "--"
+let smtp_pass = process.env.SMTP_PASS || "--"
+
+let transport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: smtp_login,
+        pass: smtp_pass
+    }
+})
+
+export const sendMessage = async (body: RequestBodyType) => {
+    const {name, email, subject, message} = body
+    await transport.sendMail({
+        from: name,
+        to: "kelekofficial@gmail.com",
+        subject: "HR WANTS ME",
+        html: `<div style="width: 300px; height: 400px; background-color: #9890C7; padding: 25px;">
+                <div style="height: 100%;">
+                    <h3 style="color: #0c0c0c; text-align: center;">New Message!!</h3>
+                    <span style="display: block; margin-bottom: 20px;">Name: ${name}</span>
+                    <span style="display: block; margin-bottom: 20px;">Subject: ${subject}</span>
+                    <span style="display: block; margin-bottom: 20px;">Email: ${email}</span>
+                    <p style="color: #0c0c0c;">Message: ${message}</p>
+                </div>
+            </div>`
+    })
+}
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import express from "express"
+import cors from "cors"
+import bodyParser from "body-parser";
+import {sendMessage} from "./Features/SendMessage/send";
+
+const app = express()
+const PORT = process.env.PORT || 7777;
+app.use(cors())
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+type UserType = {
+    id: string
+    name: string
+    status: string | null
+}
+type UsersType = {
+    users: UserType[]
+}
+const USERS: UsersType = {
+    users: [
+        {id: "1", name: "Kelek", status: null},
+        {id: "2", name: "Ira", status: null},
+        {id: "3", name: "Vitalik", status: null},
+        {id: "4", name: "Artur", status: null}
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(
+        `<h2>Welcome to test server by Kelek</h2>
+         <div>My contacts: </div>
+         <div>Email: kelekofficial@gmail.com</div> 
+         <div>Telegram: <a href="https://t.me/KelekOfficial">https://t.me/KelekOfficial</a></div>`
+    )
+})
+
+app.get('/users', (req, res) => {
+    res.status(200).json(USERS)
+    res.send(USERS)
+})
+
+app.get(`/users/:userID`, (req, res) => {
+    const user = USERS.users.find(u => u.id === req.params.userID)
+    if(user) {
+        res.status(200).json(user)
+        res.send(user)
+    } else {
+        res.send("User not found")
+    }
+})
+
+app.post('/send', async (req, res) => {
+    await sendMessage(req.body)
+    res.send("Ok")
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
