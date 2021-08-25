670c1c4008ccff5791772f85aefe9b8fa3ffefa7
Dmitry Kelek <kelekofficial@gmail.com>
2021-08-25 22:57:29 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import {sendMessage} from "./Features/SendMessage/send";
 const app = express()
 const PORT = process.env.PORT || 7777;
 app.use(cors({
-    origin: "https://dimakelek.github.io/portfolio"
+    origin: "https://dimakelek.github.io"
 }))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
