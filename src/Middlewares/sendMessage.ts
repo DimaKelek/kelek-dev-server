fc1c77902e079bc66d58032fd986f1195925d061
Dmitry Kelek <kelekofficial@gmail.com>
2021-08-28 00:22:17 +0300

    has been created requests middleware

--- src/Middlewares/sendMessage.ts
+++ src/Middlewares/sendMessage.ts
@@ -1,13 +1,14 @@
 import {RequestBodyType} from "../Features/SendMessage/send";
 
 export const requestsHandler = (req: RequestBodyType, e?: any) => {
-    let errors: string[] = []
+    let errors = []
     let statusCode: number
     if(e.code) {
         statusCode = 1
-        if (e.code === "EAUTH") {
-            errors.push("535-5.7.8 Username and Password not accepted.")
-        }
+        // if (e.code === "EAUTH") {
+        //     errors.push("535-5.7.8 Username and Password not accepted.")
+        // }
+        errors.push(e)
     } else {
         statusCode = 0
     }
