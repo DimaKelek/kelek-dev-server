72b50fffdf0d5e5e166c49a56e4fc86d7562c5c9
Dmitry Kelek <kelekofficial@gmail.com>
2021-08-27 15:37:00 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -66,9 +66,14 @@ app.get(`/users/:userID`, (req, res) => {
 })
 
 app.post('/send', (req, res) => {
-    sendMessage(req.body)
-    res.status(200).json({})
-    res.send("Ok")
+    try {
+        sendMessage(req.body)
+        res.status(200).json({})
+        res.send("Ok")
+    } catch (e) {
+        res.status(501).json({errors: e})
+    }
+
 })
 
 app.listen(PORT, () => {
